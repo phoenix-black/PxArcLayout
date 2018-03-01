999e8f8f062e1e8ea3c1690bfa1e166d3d252aa6
Praba <prabalinux@gmail.com>
2018-03-01 19:48:38 +0530

    Added Animation to ArcLayout 1) showMenu(View,View) 2) hideMenu(View,View)

---
+++ app/src/main/java/com/blackphoenix/pxarclayout/AnimUtils.java
@@ -0,0 +1,132 @@
+package com.blackphoenix.pxarclayout;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.view.View;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.OvershootInterpolator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Praba on 3/1/2018.
+ */
+public class AnimUtils {
+
+    public static final String ROTATION = "rotation";
+    public static final String SCALE_X = "scaleX";
+    public static final String SCALE_Y = "scaleY";
+    public static final String TRANSLATION_X = "translationX";
+    public static final String TRANSLATION_Y = "translationY";
+
+    private AnimUtils() {
+        //No instances.
+    }
+
+    public static PropertyValuesHolder rotation(float... values) {
+        return PropertyValuesHolder.ofFloat(ROTATION, values);
+    }
+
+    public static PropertyValuesHolder translationX(float... values) {
+        return PropertyValuesHolder.ofFloat(TRANSLATION_X, values);
+    }
+
+    public static PropertyValuesHolder translationY(float... values) {
+        return PropertyValuesHolder.ofFloat(TRANSLATION_Y, values);
+    }
+
+    public static PropertyValuesHolder scaleX(float... values) {
+        return PropertyValuesHolder.ofFloat(SCALE_X, values);
+    }
+
+    public static PropertyValuesHolder scaleY(float... values) {
+        return PropertyValuesHolder.ofFloat(SCALE_Y, values);
+    }
+
+//    @SuppressWarnings("NewApi")
+    public static void showMenu(View menuLayout, ArcLayout arcLayout, View fab) {
+        menuLayout.setVisibility(View.VISIBLE);
+
+        List<Animator> animList = new ArrayList<>();
+
+        for (int i = 0, len = arcLayout.getChildCount(); i < len; i++) {
+            animList.add(createShowItemAnimator(arcLayout.getChildAt(i), fab));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(400);
+        animSet.setInterpolator(new OvershootInterpolator());
+        animSet.playTogether(animList);
+        animSet.start();
+    }
+
+ //   @SuppressWarnings("NewApi")
+    public static void hideMenu(final View menuLayout, ArcLayout arcLayout, View fab) {
+
+        List<Animator> animList = new ArrayList<>();
+
+        for (int i = arcLayout.getChildCount() - 1; i >= 0; i--) {
+            animList.add(createHideItemAnimator(arcLayout.getChildAt(i), fab));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(400);
+        animSet.setInterpolator(new AnticipateInterpolator());
+        animSet.playTogether(animList);
+        animSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                menuLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+        animSet.start();
+
+    }
+
+    public static Animator createShowItemAnimator(View item, View fab) {
+
+        float dx = fab.getX() - item.getX();
+        float dy = fab.getY() - item.getY();
+
+        item.setRotation(0f);
+        item.setTranslationX(dx);
+        item.setTranslationY(dy);
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimUtils.rotation(0f, 720f),
+                AnimUtils.translationX(dx, 0f),
+                AnimUtils.translationY(dy, 0f)
+        );
+
+        return anim;
+    }
+
+    public static Animator createHideItemAnimator(final View item, View fab) {
+        float dx = fab.getX() - item.getX();
+        float dy = fab.getY() - item.getY();
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimUtils.rotation(720f, 0f),
+                AnimUtils.translationX(0f, dx),
+                AnimUtils.translationY(0f, dy)
+        );
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                item.setTranslationX(0f);
+                item.setTranslationY(0f);
+            }
+        });
+
+        return anim;
+    }
+}
